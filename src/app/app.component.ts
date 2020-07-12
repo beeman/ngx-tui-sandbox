14d36d9621f18f63f68dee50fccf26e8a1b0d27a
Bram Borggreve <borggreve@gmail.com>
2020-07-12 12:52:54 -0500

    feat: add basic demo of ngx-tui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { TuiEditorOptions } from 'ngx-tui';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'sandbox';
+  options: TuiEditorOptions = {
+    initialValue: '# Hello World',
+    initialEditType: 'markdown',
+    previewStyle: 'vertical',
+    height: 'auto',
+    minHeight: '500px',
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { TuiModule } from 'ngx-tui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, TuiModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
